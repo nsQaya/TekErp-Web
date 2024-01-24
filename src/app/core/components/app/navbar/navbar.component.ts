ad4698164117c54795b17473817e956c19be8bd7
nsQaya <nskaya@outlook.com>
2024-01-24 10:12:53 +0300

    navbar hamburger menu eklentisi

--- src/app/core/components/app/navbar/navbar.component.ts
+++ src/app/core/components/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { ADMINROUTES } from '../sidebar/sidebar.component';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../admin/login/services/auth.service';
 import { SharedService } from 'app/core/services/shared.service';
@@ -12,9 +14,15 @@ import { SharedService } from 'app/core/services/shared.service';
 })
 export class NavbarComponent implements OnInit {
 
+	private listTitles: any[];
+	private toggleButton: any;
+	location: Location;
+	mobile_menu_visible: any = 0;
+	private sidebarVisible: boolean;
+
 	userName: string;
 	clickEventSubscription:Subscription;
-	constructor(private authService: AuthService, private router: Router, private sharedService:SharedService) {
+	constructor(private authService: AuthService, private router: Router, private sharedService:SharedService,private element: ElementRef,) {
 
 		this.clickEventSubscription= this.sharedService.getChangeUserNameClickEvent().subscribe(()=>{
 			this.setUserName();
@@ -43,7 +51,106 @@ export class NavbarComponent implements OnInit {
 	ngOnInit() {
 		console.log(this.userName);
 		this.userName = this.authService.getUserName();
-	}
+		this.listTitles = ADMINROUTES.filter(listTitle => listTitle);
+      const navbar: HTMLElement = this.element.nativeElement;
+      this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+      this.router.events.subscribe((event) => {
+        this.sidebarClose();
+         var $layer: any = document.getElementsByClassName('close-layer')[0];
+         if ($layer) {
+           $layer.remove();
+           this.mobile_menu_visible = 0;
+         }
+     });
+	};
+
+	sidebarOpen() {
+        const toggleButton = this.toggleButton;
+        const body = document.getElementsByTagName('body')[0];
+        setTimeout(function(){
+            toggleButton.classList.add('toggled');
+        }, 500);
+
+        body.classList.add('nav-open');
+
+        this.sidebarVisible = true;
+    };
+    sidebarClose() {
+        const body = document.getElementsByTagName('body')[0];
+        this.toggleButton.classList.remove('toggled');
+        this.sidebarVisible = false;
+        body.classList.remove('nav-open');
+    };
+    sidebarToggle() {
+        // const toggleButton = this.toggleButton;
+        // const body = document.getElementsByTagName('body')[0];
+        var $toggle = document.getElementsByClassName('navbar-toggler')[0];
+
+        if (this.sidebarVisible === false) {
+            this.sidebarOpen();
+        } else {
+            this.sidebarClose();
+        }
+        const body = document.getElementsByTagName('body')[0];
+
+        if (this.mobile_menu_visible == 1) {
+            // $('html').removeClass('nav-open');
+            body.classList.remove('nav-open');
+            if ($layer) {
+                $layer.remove();
+            }
+            setTimeout(function() {
+                $toggle.classList.remove('toggled');
+            }, 400);
+
+            this.mobile_menu_visible = 0;
+        } else {
+            setTimeout(function() {
+                $toggle.classList.add('toggled');
+            }, 430);
+
+            var $layer = document.createElement('div');
+            $layer.setAttribute('class', 'close-layer');
+
+
+            if (body.querySelectorAll('.main-panel')) {
+                document.getElementsByClassName('main-panel')[0].appendChild($layer);
+            }else if (body.classList.contains('off-canvas-sidebar')) {
+                document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
+            }
+
+            setTimeout(function() {
+                $layer.classList.add('visible');
+            }, 100);
+
+            $layer.onclick = function() { //asign a function
+              body.classList.remove('nav-open');
+              this.mobile_menu_visible = 0;
+              $layer.classList.remove('visible');
+              setTimeout(function() {
+                  $layer.remove();
+                  $toggle.classList.remove('toggled');
+              }, 400);
+            }.bind(this);
+
+            body.classList.add('nav-open');
+            this.mobile_menu_visible = 1;
+
+        }
+    };
+	getTitle(){
+		var titlee = this.location.prepareExternalUrl(this.location.path());
+		if(titlee.charAt(0) === '#'){
+			titlee = titlee.slice( 1 );
+		}
+  
+		for(var item = 0; item < this.listTitles.length; item++){
+			if(this.listTitles[item].path === titlee){
+				return this.listTitles[item].title;
+			}
+		}
+		return 'Dashboard';
+	  }
 
 	setUserName(){
 
